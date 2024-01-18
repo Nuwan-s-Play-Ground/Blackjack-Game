18d5687222dee2a284fb41f79ed993f8089dd9e8
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-01-18 19:03:58 +0530

    Solving our cards problem with an array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let firstCard = 20;
-let secondCard = 1;
+let firstCard = 10;
+let secondCard = 4;
+let cards=[firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
@@ -9,8 +10,12 @@ let messageEl = document.getElementById("message-el");
 let sumEl=document.getElementById("sum-el");
 let cardsEl=document.getElementById("cards-el");
 
-function startGame() {
-   cardsEl.textContent="Cards:" + firstCard + " " + secondCard;
+function startGame(){
+    renderGame();
+}
+
+function renderGame() {
+   cardsEl.textContent="Cards:" + cards[0] + " " + cards[1];
    sumEl.textContent="Sum:" + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
